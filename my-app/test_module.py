44486d73206d2ef47dc31032c779c8fb50e0ad81
Nathanael Yao <18ny13@queensu.ca>
2025-04-24 13:49:04 -0400

    add good example

--- my-app/test_module.py
+++ my-app/test_module.py
@@ -5,7 +5,7 @@ class Solution:
     A class to generate an array of n unique integers that sum up to 0.
     """
     
-    def sumZero(self, n: int):
+    def sumZero(self, n: int) -> List[int]:
         """
         Returns a list of n integers such that their sum is zero.
         
@@ -16,7 +16,7 @@ class Solution:
         
         Returns
         -------
-        : 
+        List[int] 
             A list of n integers where the sum of the integers is zero.
         """
         
@@ -30,7 +30,7 @@ class Solution:
         res.append(-prefix_sum)
         return res
 
-    def sumZero2(self, n: int):
+    def sumZero2(self, n: int) -> List[int]:
         """
         Returns a list of n integers such that their sum is zero.
         
@@ -115,6 +115,31 @@ class Solution:
         -------
         List[int]
         A list of n integers where the sum of the integers is zero.
+        """
+        
+        prefix_sum = 0
+        res = []
+        # 1, n-1
+        for i in range(1, n):
+            res.append(i)
+            prefix_sum = prefix_sum + i
+        # sum(from 1 to n-1)
+        res.append(-prefix_sum)
+        return res
+    
+    def sumZero6(self, n: int):
+        """
+        Returns a list of n integers such that their sum is zero.
+        
+        Parameters
+        ----------
+        n
+            The number of integers to be returned.
+        
+        Returns
+        -------
+        : 
+            A list of n integers where the sum of the integers is zero.
         """
         
         prefix_sum = 0
